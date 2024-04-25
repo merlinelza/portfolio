dd244b7df7a756dc5d88d6467f56d65d170d4fdc
merlinelza <122732248+merlinelza@users.noreply.github.com>
2024-04-25 19:54:12 +0530

    third

--- src/componets/About/about.js
+++ src/componets/About/about.js
@@ -1,36 +1,44 @@
 import React from 'react';
 import './about.css';
+import myPhoto from '../../logo/myPhoto.jpg';
+import { FaAward } from 'react-icons/fa6';
+import { RiFolderOpenLine } from 'react-icons/ri';
 
 const About = () => {
     return (
         <section id="about">
-            <span className="aboutTitle">What I do</span> <br />
-            <span className="aboutDesp">
-                As a Full Stack Developer, I am fueled by a passion for
-                problem-solving and possess a diverse skill set in web
-                development.
+            <p className="about_Intro">Get to know</p>
+            <h2 className="about_heading">About me</h2>
+            <div className="about_container">
+                <div className="about_me">
+                    <div className="about_me_image">
+                        <img src={myPhoto} alt="myPhoto" />
+                    </div>
+                </div>
+            </div>
+            <div className="about_content">
+                <div className="about_cards">
+                    <article className="about_card">
+                        <FaAward className="about_cardIcon" />
+                        <h5>Experience</h5>
+                        <small>1 Years</small>
+                    </article>
+                    <article className="about_card">
+                        <RiFolderOpenLine className="about_cardIcon" />
+                        <h5>Projects</h5>
+                        <small>3+ Completed</small>
+                    </article>
+                </div>
                 <br />
-                With a strong foundation in computer science, I have refined my
-                expertise in front-end technologies like React and Vue.js,
-                Enhanced by back-end proficiency in Node.js. I thrive in
-                collaborative environments, embracing agile practices to create
+                <p className="aboutPara">
+                    As a Full Stack Developer, I am fueled by a passion for
+                    problem-solving and possess a diverse skill set in Web
+                    Development.
+                </p>
                 <br />
-                seamless user experiences and powerful APIs. Continuously
-                learning and staying up-to-date with emerging trends, I am
-                committed to leaving a positive impact on end-users and shaping
-                the future of software engineering with my innovative solutions.
-                With a blend of creativity and technical prowess, I strive to
-                craft elegant solutions that elevate user experiences. <br />
-                With a passion for pushing the boundaries of what's possible in
-                web development, I approach each project with a dedication to
-                excellence and a drive to exceed expectations. With a keen eye
-                for detail and a commitment to delivering solutions that not
-                only meet but exceed client requirements, I thrive on turning
-                ideas into reality and transforming visions into tangible
-                results.
-            </span>
+                <a href="#contactMe">Let's Talk</a>
+            </div>
         </section>
     );
 };
-
 export default About;
